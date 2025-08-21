9a83d4eac047739a49b3c72661eee7064811e2c0
lalith charan <lalithcharan111@gmail.com>
2025-08-21 16:13:41 +0530

    Create custom.py

---
+++ custom.py
@@ -0,0 +1,24 @@
+import pandas as pd
+
+data = []
+
+while True:
+    name = input("Enter student name: ")
+    roll = input("Enter roll number: ")
+    maths = int(input("Enter Maths marks: "))
+    science = int(input("Enter Science marks: "))
+    english = int(input("Enter English marks: "))
+    attendance = int(input("Enter attendance percentage: "))
+
+    data.append([name, roll, maths, science, english, attendance])
+
+    cont = input("Do you want to add another student? (y/n): ")
+    if cont.lower() != 'y':
+        break
+
+df = pd.DataFrame(data, columns=['Name', 'Roll No', 'Maths', 'Science', 'English', 'Attendance'])
+print("\nFinal Report:")
+print(df)
+
+df.to_csv("student_data.csv", index=False)
+print("\nData saved to student_data.csv")
